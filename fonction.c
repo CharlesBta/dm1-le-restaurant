a1ac212e9159c407b661da3b24bc7d39bca8d752
Charles-Antoine <charto.bta@gmail.com>
2023-09-07 17:43:01 +0200

    v0.2

--- fonction.c
+++ fonction.c
@@ -4,3 +4,24 @@ int numTable(int value){
     else if(value<=8)return 3;
     return 0;
 }
+int choixplat(char value){
+    if(value=='c')return 15;
+    else if (value=='t')return 8;
+    else if (value=='r')return 23;
+    return 1;
+}
+int choixBoisson(char value){
+    if(value=='s')return 5;
+    else if (value=='e')return 2;
+    else if (value=='l')return 12;
+    return 1;
+}
+int choixDessert(char value){
+    if(value=='t')return 62;
+    else if (value=='g')return 3;
+    else if (value=='m')return 8;
+    return 1;
+}
+int sommePlat(char boi,char pl,char des,int nbClient){
+    return nbClient*(choixplat(pl)+ choixBoisson(boi)+ choixDessert(des));
+}
--- main.c
+++ main.c
@@ -2,10 +2,29 @@
 #include "fonction.c"
 
 int main() {
+    int nomClient;
     int table;
+    char boisson;
+    char plat;
+    char dessert;
+    int prixTotal;
+
     printf("Combien etes vous pour le restaurant? \n");
-    scanf("%d",&table);
-    table = numTable(table);
-    printf("%d",table);
+    scanf("%d",&nomClient);
+    table = numTable(nomClient);
+    printf("%d\n",table);
+
+    printf("Quel boisson souhaitez vous? (s: soda, e: eau, l: limonade\n");
+    scanf("%c", &boisson);
+    getchar();
+    printf("Quel plats ? (c: coucous, t: tomates, r: risotto\n");
+    scanf("%c", &plat);
+    getchar();
+    printf("Quel dessert? (t: tiramisu, g: glace, m: macaron\n");
+    scanf("%c", &dessert);
+    getchar();
+
+    prixTotal = sommePlat(boisson,plat,dessert,nomClient);
+    printf("%d",prixTotal);
     return 0;
 }
