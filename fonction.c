fc2c37390779561f51c315e76839e1415451494b
Charles-Antoine <charto.bta@gmail.com>
2023-09-07 14:55:52 +0200

    v0.1
    fonction choix de la table et demande à l'user

---
+++ fonction.c
@@ -0,0 +1,6 @@
+int numTable(int value){
+    if(value<=2)return 1;
+    else if(value<=4)return 2;
+    else if(value<=8)return 3;
+    return 0;
+}
--- main.c
+++ main.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
+#include "fonction.c"
 
 int main() {
-    printf("Hello, World!\n");
+    int table;
+    printf("Combien etes vous pour le restaurant? \n");
+    scanf("%d",&table);
+    table = numTable(table);
+    printf("%d",table);
     return 0;
 }
